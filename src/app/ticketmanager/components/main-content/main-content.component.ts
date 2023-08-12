b90ac9da0aac2586ead8e0091c7c699c0d0e39d2
CosmicPhoenix2029 <haydnatkinson6@gmail.com>
2023-08-12 19:33:42 +0100

    added a bit of data to ticket.json

--- src/app/ticketmanager/components/main-content/main-content.component.ts
+++ src/app/ticketmanager/components/main-content/main-content.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TicketService } from '../../services/ticket.service';
 
 @Component({
   selector: 'app-main-content',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class MainContentComponent {
 
+  constructor(private ticketService: TicketService) {}
+
+  ngOnInit(): void {
+
+  }
 }
--- src/app/ticketmanager/services/ticket.service.ts
+++ src/app/ticketmanager/services/ticket.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { ticket } from '../models/ticket';
 
 @Injectable({
   providedIn: 'root'
 })
+
+@Injectable({
+  providedIn: "root"
+})
+
 export class TicketService {
 
   constructor() { }
+
+  getTickets(): ticket[] {
+    return [];
+  }
 }
