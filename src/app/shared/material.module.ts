984129ea14475241a22c19777665f90d9c106756
CosmicPhoenix2029 <haydnatkinson6@gmail.com>
2023-08-12 16:30:54 +0100

    added icons to the sidenav

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatSidenavModule} from '@angular/material/sidenav';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   exports: [
-    MatSidenavModule,        
+    MatSidenavModule,  
+    MatToolbarModule,
+    MatListModule,  
+    MatIconModule,    
   ]
 })
 export class MaterialModule { }
