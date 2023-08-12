60098b387040543bfcc251cfef393bf55976f61e
CosmicPhoenix2029 <haydnatkinson6@gmail.com>
2023-08-12 15:26:10 +0100

    configured routing for both root and children

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material.module';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  //lazy load the module if /ticketmanager is navigated to
+  { path: 'ticketmanager', loadChildren: () => import('./ticketmanager/ticketmanager.module').then(m => m.TicketmanagerModule ) },
+  //catch all route to redirect to the above route
+  { path: '**', redirectTo: 'ticketmanager' }
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +22,8 @@ import { MaterialModule } from './shared/material.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/ticketmanager/ticketmanager.module.ts
+++ src/app/ticketmanager/ticketmanager.module.ts
@@ -4,8 +4,20 @@ import { TicketmanagerAppComponent } from './ticketmanager-app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '../shared/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
-
+const routes: Routes = [
+  //default route for the app
+  {path: '', component: TicketmanagerAppComponent, children: [
+    //default route for the TicketManagerAppComponents sub components
+      {path: '', component: MainContentComponent}
+    ]
+  },
+  { path: '**', redirectTo: '' } 
+];
 
 @NgModule({
   declarations: [
@@ -15,7 +27,11 @@ import { MainContentComponent } from './components/main-content/main-content.com
     MainContentComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class TicketmanagerModule { }
