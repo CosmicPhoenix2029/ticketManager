9c0e8c080ee530fd5afd9afc907acadc99009a12
CosmicPhoenix2029 <haydnatkinson6@gmail.com>
2023-08-12 19:15:13 +0100

    created a ticket service

--- src/app/ticketmanager/ticketmanager.module.ts
+++ src/app/ticketmanager/ticketmanager.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TicketmanagerAppComponent } from './ticketmanager-app.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { SidenavComponent } from './components/sidenav/sidenav.component';
-import { MainContentComponent } from './components/main-content/main-content.component';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '../shared/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { TicketmanagerAppComponent } from './ticketmanager-app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { TicketService } from './services/ticket.service';
 
 const routes: Routes = [
   //default route for the app
@@ -32,6 +34,9 @@ const routes: Routes = [
     MaterialModule,
     FlexLayoutModule,
     RouterModule.forChild(routes)
+  ],
+  providers: [
+    TicketService
   ]
 })
 export class TicketmanagerModule { }
